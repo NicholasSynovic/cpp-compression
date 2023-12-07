7db1286f6eb5226907dc5700c1122a6df95474e8
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-07 11:54:05 -0600

    Add CLI11 command parsing

--- compression/serial_compression.cpp
+++ compression/serial_compression.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <CLI/CLI.hpp>
 
-int main()  {
-    std::cout << "Hello world";
+int main(int argc, char **argv)  {
+    int numberOfBits = 0;
+
+    CLI::App app{"Bloom Filter Implementation"};
+    app.option_defaults()->always_capture_default(true);
+
+    app.add_option("-b,--number-of-bits", numberOfBits,
+                 "Number of bits to allocate to the bit vector")
+      ->check(CLI::PositiveNumber.description(" >0"));
+    
+    CLI11_PARSE(app, argc, argv);
+    
     return 0;
 }
 
