32a718b97482981700c8cb4fa2ffd64d9066f68d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-07 17:21:06 -0600

    Start work on benchmarking project

---
+++ benchmark.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Benchmarking script
+# Each program is ran 100 times and times are saved to 
+# benchmarks/$PROGRAM_NAME.csv
+
+# To keep everything fair, the maximum compression ratio is choosen for each 
+# compressor
+
+mkdir benchmarks
+
+# Tar Compressor (Serial)
+
+# BZip2 Compressor (Serial)
+
+# Serial Compressor (Serial)
+
+# LBZip2 Compressor (Parallel)
+
+# OpenMP Parallel Compressor (Parallel)
