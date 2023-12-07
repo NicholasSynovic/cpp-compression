053310a7d0cf9b92de9d3a866877cb43e2d74a8d
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-07 16:19:31 -0600

    Seperate compression code from the original implementation

--- src/build.bash
+++ src/build.bash
@@ -2,4 +2,4 @@
 
 cmake .
 cmake --build .
-#rm -rf CMakeFiles _deps Makefile CPackConfig.cmake CPackSourceConfig.cmake CMakeCache.txt cmake_install.cmake CPackConfig.cmake CPackSourceConfig.cmake
+# rm -rf CMakeFiles _deps Makefile CPackConfig.cmake CPackSourceConfig.cmake CMakeCache.txt cmake_install.cmake CPackConfig.cmake CPackSourceConfig.cmake
