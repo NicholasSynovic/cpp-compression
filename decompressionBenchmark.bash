0e0ba77cd95123fb0e24b50a5415e268e2e79904
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-09 12:46:33 -0600

    Complete decompression benchmark

--- decompressionBenchmark.bash
+++ decompressionBenchmark.bash
@@ -25,16 +25,19 @@ mkdir -p $OUTPUT_BENCHMARK_DIR
 
 # Delete $BZIP2_ARCHIVE_FILEPATH
 # Delete $BZIP2_BACKUP_ARCHIVE_FILEPATH
-rm $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH
+rm --force $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH
+
+# Copy $BACKUP_OUTPUT_FILEPATH to $OUTPUT_FILEPATH
+cp $BACKUP_OUTPUT_FILEPATH $OUTPUT_FILEPATH
 
 # Create $BZIP2_ARCHIVE_FILEPATH
-src/bin/serial_compression -i $OUTPUT_FILE -o $BZIP2_ARCHIVE_FILEPATH
+src/bin/serial_compression -i $OUTPUT_FILEPATH -o $BZIP2_ARCHIVE_FILEPATH
 
 # Create $BZIP2_BACKUP_ARCHIVE_FILEPATH
 cp $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH
 
-# Delete $OUTPUT_FILENAME
-rm $OUTPUT_FILEPATH
+# Delete $OUTPUT_FILEPATH
+rm --force $OUTPUT_FILEPATH
 
 # Run benchmarks
 
@@ -51,50 +54,50 @@ rm $OUTPUT_FILEPATH
 # do
 #     echo "Tar $ACTION_WORD: $i / $MAX_ITER"
 #     cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
-#     /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/tar.csv --format=$TIME_FORMAT --quiet tar -jxf $INPUT_FILE -C "testData"
+#     /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/tar.csv --format=$TIME_FORMAT --quiet tar -jxf $BZIP2_ARCHIVE_FILEPATH -C "testData"
 #     rm $OUTPUT_FILEPATH
 # done
 
 # BZip2 Decompression (Serial)
 for i in $(seq 1 $MAX_ITER);
 do
-    echo "BZip 2 $ACTION_WORD: $i / $MAX_ITER"
-    cp $BACKUP_FILE $INPUT_FILE
-    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --decompress --keep --force --quiet $INPUT_FILE
-    rm $OUTPUT_FILE
+    echo "BZip2 $ACTION_WORD: $i / $MAX_ITER"
+    cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --decompress --keep --force --quiet $BZIP2_ARCHIVE_FILEPATH
+    rm $OUTPUT_FILEPATH
 done
 
 # Serial Decompression (Serial)
 for i in $(seq 1 $MAX_ITER);
 do
     echo "Serial $ACTION_WORD: $i / $MAX_ITER"
-    cp $BACKUP_FILE $INPUT_FILE
-    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_decompression -i $INPUT_FILE -o $OUTPUT_FILE
-    rm $OUTPUT_FILE
+    cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_decompression -i $BZIP2_ARCHIVE_FILEPATH -o $OUTPUT_FILEPATH
+    rm $OUTPUT_FILEPATH
 done
 
 # LBZip2 Decompression (Parallel)
 for i in $(seq 1 $MAX_ITER);
 do
     echo "LBZip2 $ACTION_WORD: $i / $MAX_ITER"
-    cp $BACKUP_FILE $INPUT_FILE
-    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --decompress --force --quiet $INPUT_FILE
-    rm $OUTPUT_FILE
+    cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --decompress --force --quiet $BZIP2_ARCHIVE_FILEPATH
+    rm $OUTPUT_FILEPATH
 done
 
 # OpenMP Decompression (Parallel)
 for i in $(seq 1 $MAX_ITER);
 do
     echo "OpenMP $ACTION_WORD: $i / $MAX_ITER"
-    cp $BACKUP_FILE $INPUT_FILE
-    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_decompression -i $INPUT_FILE -o $OUTPUT_FILE
-    rm $OUTPUT_FILE
+    cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_decompression -i $BZIP2_ARCHIVE_FILEPATH -o $OUTPUT_FILEPATH
+    rm $OUTPUT_FILEPATH
 done
 
 # Delete $BZIP2_ARCHIVE_FILEPATH
 # Delete $BZIP2_BACKUP_ARCHIVE_FILEPATH
-# Delete $OUTPUT_FILENAME
-rm $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH $OUTPUT_FILEPATH
+# Delete $OUTPUT_FILEPATH
+rm --force $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH $OUTPUT_FILEPATH
 
 # Copy $BACKUP_OUTPUT_FILEPATH to $OUTPUT_FILEPATH
 cp $BACKUP_OUTPUT_FILEPATH $OUTPUT_FILEPATH
