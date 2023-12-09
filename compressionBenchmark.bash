6c50f770aeb81df0b0298e695751b90ac3a0b558
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-09 11:18:52 -0600

    Add file recovery safety control

--- compressionBenchmark.bash
+++ compressionBenchmark.bash
@@ -10,6 +10,9 @@
 TIME_FORMAT="%S,%U,%e"
 OUTPUT_DIR="benchmarks/compression"
 WORD="Compression"
+INPUT_FILE="testData/enwik8"
+OUTPUT_FILE="testData/enwik8.bz2"
+BACKUP_FILE="testData/enwik8.bk"
 
 mkdir -p $OUTPUT_DIR
 
@@ -17,38 +20,48 @@ mkdir -p $OUTPUT_DIR
 for i in $(seq 1 10);
 do
     echo "Tar $WORD: $i / 10"
-    /usr/bin/time --append --output $OUTPUT_DIR/tar.csv --format=$TIME_FORMAT --quiet tar -cjf testData/enwik8.bz2 testData/enwik8
-    rm testData/enwik8.bz2
+    # Rest benchmarking file for safety
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_DIR/tar.csv --format=$TIME_FORMAT --quiet tar -cjf $OUTPUT_FILE $INPUT_FILE
+    rm $OUTPUT_FILE
 done
 
 # BZip2 Compression (Serial)
 for i in $(seq 1 10);
 do
     echo "BZip 2 $WORD: $i / 10"
-    /usr/bin/time --append --output $OUTPUT_DIR/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --compress --keep --force --quiet --best testData/enwik8 testData/enwik8.bz2 
-    rm testData/enwik8.bz2
+    # Rest benchmarking file for safety
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_DIR/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --compress --keep --force --quiet --best $INPUT_FILE $OUTPUT_FILE
+    rm $OUTPUT_FILE
 done
 
 # Serial Compression (Serial)
 for i in $(seq 1 10);
 do
     echo "Serial $WORD: $i / 10"
-    /usr/bin/time --append --output $OUTPUT_DIR/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_compression -i testData/enwik8 -o testData/enwik8.bz2 
-    rm testData/enwik8.bz2
+    # Rest benchmarking file for safety
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_DIR/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_compression -i $INPUT_FILE -o $OUTPUT_FILE
+    rm $OUTPUT_FILE
 done
 
 # LBZip2 Compression (Parallel)
 for i in $(seq 1 10);
 do
     echo "LBZip2 $WORD: $i / 10"
-    /usr/bin/time --append --output $OUTPUT_DIR/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --compress --best --force testData/enwik8
-    rm testData/enwik8.bz2
+    # Rest benchmarking file for safety
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_DIR/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --compress --best --force $INPUT_FILE
+    rm $OUTPUT_FILE
 done
 
 # OpenMP Compression (Parallel)
 for i in $(seq 1 10);
 do
     echo "OpenMP $WORD: $i / 10"
-    /usr/bin/time --append --output $OUTPUT_DIR/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_compression -i testData/enwik8 -o testData/enwik8.bz2 
-    rm testData/enwik8.bz2
+    # Rest benchmarking file for safety
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_DIR/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_compression -i $INPUT_FILE -o $OUTPUT_FILE
+    rm $OUTPUT_FILE
 done
