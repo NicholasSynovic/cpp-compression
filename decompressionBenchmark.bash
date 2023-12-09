86bc351857697b9272621ea772d786f9f1efb925
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-09 12:30:40 -0600

    Start work on decompression benchmark

--- decompressionBenchmark.bash
+++ decompressionBenchmark.bash
@@ -1,20 +1,100 @@
 #!/bin/bash
 
 # Benchmarking script
-# Each program is ran 100 times and times are saved to 
+# Each program is ran $MAX_ITER times and times are saved to 
 # benchmarks/$PROGRAM_NAME.csv
 
 # To keep everything fair, the maximum compression ratio is choosen for each 
 # compressor
 
-mkdir benchmarks
+# CHANGE THESE TO ADJUST BENCHMARK FUNCTIONALITY
+MAX_ITER=10
+BZIP2_ARCHIVE_FILEPATH="testData/enwik8.bz2"
+OUTPUT_BENCHMARK_DIR="benchmarks/decompression"
+OUTPUT_FILEPATH="testData/enwik8"
+# ====
 
-# Tar Compressor (Serial)
+ACTION_WORD="Decompression"
+TIME_FORMAT="%S,%U,%e"
 
-# BZip2 Compressor (Serial)
+BACKUP_OUTPUT_FILEPATH="$OUTPUT_FILEPATH.bk"
+BZIP2_BACKUP_ARCHIVE_FILEPATH="$BZIP2_ARCHIVE_FILEPATH.bk"
 
-# Serial Compressor (Serial)
+# Create $OUTPUT_BENCHMARK_DIR
+mkdir -p $OUTPUT_BENCHMARK_DIR
 
-# LBZip2 Compressor (Parallel)
+# Delete $BZIP2_ARCHIVE_FILEPATH
+# Delete $BZIP2_BACKUP_ARCHIVE_FILEPATH
+rm $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH
 
-# OpenMP Parallel Compressor (Parallel)
+# Create $BZIP2_ARCHIVE_FILEPATH
+src/bin/serial_compression -i $OUTPUT_FILE -o $BZIP2_ARCHIVE_FILEPATH
+
+# Create $BZIP2_BACKUP_ARCHIVE_FILEPATH
+cp $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH
+
+# Delete $OUTPUT_FILENAME
+rm $OUTPUT_FILEPATH
+
+# Run benchmarks
+
+# Example benchmark
+# for i in range(1 - $MAX_ITER)
+#   echo "PROGRAM_NAME $ACTION_WORD: $i / $MAX_ITER
+#   cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
+#   /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/PROGRAM_NAME.csv --format=$TIME_FORMAT --quiet PROGRAM_NAME PROGRAM_OPTIONS
+#   rm $OUTPUT_FILEPATH
+
+# Tar Decompression (Serial)
+# NOTE: Tar does not decompress .bz2 archives, needs .tar.bz2 archives
+# for i in $(seq 1 $MAX_ITER);
+# do
+#     echo "Tar $ACTION_WORD: $i / $MAX_ITER"
+#     cp $BZIP2_BACKUP_ARCHIVE_FILEPATH $BZIP2_ARCHIVE_FILEPATH
+#     /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/tar.csv --format=$TIME_FORMAT --quiet tar -jxf $INPUT_FILE -C "testData"
+#     rm $OUTPUT_FILEPATH
+# done
+
+# BZip2 Decompression (Serial)
+for i in $(seq 1 $MAX_ITER);
+do
+    echo "BZip 2 $ACTION_WORD: $i / $MAX_ITER"
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --decompress --keep --force --quiet $INPUT_FILE
+    rm $OUTPUT_FILE
+done
+
+# Serial Decompression (Serial)
+for i in $(seq 1 $MAX_ITER);
+do
+    echo "Serial $ACTION_WORD: $i / $MAX_ITER"
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_decompression -i $INPUT_FILE -o $OUTPUT_FILE
+    rm $OUTPUT_FILE
+done
+
+# LBZip2 Decompression (Parallel)
+for i in $(seq 1 $MAX_ITER);
+do
+    echo "LBZip2 $ACTION_WORD: $i / $MAX_ITER"
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --decompress --force --quiet $INPUT_FILE
+    rm $OUTPUT_FILE
+done
+
+# OpenMP Decompression (Parallel)
+for i in $(seq 1 $MAX_ITER);
+do
+    echo "OpenMP $ACTION_WORD: $i / $MAX_ITER"
+    cp $BACKUP_FILE $INPUT_FILE
+    /usr/bin/time --append --output $OUTPUT_BENCHMARK_DIR/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_decompression -i $INPUT_FILE -o $OUTPUT_FILE
+    rm $OUTPUT_FILE
+done
+
+# Delete $BZIP2_ARCHIVE_FILEPATH
+# Delete $BZIP2_BACKUP_ARCHIVE_FILEPATH
+# Delete $OUTPUT_FILENAME
+rm $BZIP2_ARCHIVE_FILEPATH $BZIP2_BACKUP_ARCHIVE_FILEPATH $OUTPUT_FILEPATH
+
+# Copy $BACKUP_OUTPUT_FILEPATH to $OUTPUT_FILEPATH
+cp $BACKUP_OUTPUT_FILEPATH $OUTPUT_FILEPATH
