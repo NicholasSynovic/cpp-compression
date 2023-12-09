ffb972f0a914fba857148bb2d5d91a7cba940ab5
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2023-12-09 11:06:02 -0600

    Make it easier to extend program for decompression

--- compressionBenchmark.bash
+++ compressionBenchmark.bash
@@ -8,45 +8,47 @@
 # compressor
 
 TIME_FORMAT="%S,%U,%e"
+OUTPUT_DIR="benchmarks/compression"
+WORD="Compression"
 
-mkdir -p benchmarks/compression
+mkdir -p $OUTPUT_DIR
 
 # Tar Compression (Serial)
 for i in $(seq 1 10);
 do
-    echo "Tar Compression: $i / 10"
-    /usr/bin/time --append --output benchmarks/compression/tar.csv --format=$TIME_FORMAT --quiet tar -cjf testData/enwik8.bz2 testData/enwik8
+    echo "Tar $WORD: $i / 10"
+    /usr/bin/time --append --output $OUTPUT_DIR/tar.csv --format=$TIME_FORMAT --quiet tar -cjf testData/enwik8.bz2 testData/enwik8
     rm testData/enwik8.bz2
 done
 
 # BZip2 Compression (Serial)
 for i in $(seq 1 10);
 do
-    echo "BZip 2 Compression: $i / 10"
-    /usr/bin/time --append --output benchmarks/compression/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --compress --keep --force --quiet --best testData/enwik8 testData/enwik8.bz2 
+    echo "BZip 2 $WORD: $i / 10"
+    /usr/bin/time --append --output $OUTPUT_DIR/bzip2.csv --format=$TIME_FORMAT --quiet bzip2 --compress --keep --force --quiet --best testData/enwik8 testData/enwik8.bz2 
     rm testData/enwik8.bz2
 done
 
 # Serial Compression (Serial)
 for i in $(seq 1 10);
 do
-    echo "Serial Compression: $i / 10"
-    /usr/bin/time --append --output benchmarks/compression/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_compression -i testData/enwik8 -o testData/enwik8.bz2 
+    echo "Serial $WORD: $i / 10"
+    /usr/bin/time --append --output $OUTPUT_DIR/serial_compression.csv --format=$TIME_FORMAT --quiet src/bin/serial_compression -i testData/enwik8 -o testData/enwik8.bz2 
     rm testData/enwik8.bz2
 done
 
 # LBZip2 Compression (Parallel)
 for i in $(seq 1 10);
 do
-    echo "LBZip2 Compression: $i / 10"
-    /usr/bin/time --append --output benchmarks/compression/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --compress --best --force testData/enwik8
+    echo "LBZip2 $WORD: $i / 10"
+    /usr/bin/time --append --output $OUTPUT_DIR/lbzip2.csv --format=$TIME_FORMAT --quiet lbzip2 --keep --compress --best --force testData/enwik8
     rm testData/enwik8.bz2
 done
 
 # OpenMP Compression (Parallel)
 for i in $(seq 1 10);
 do
-    echo "OpenMP Compression: $i / 10"
-    /usr/bin/time --append --output benchmarks/compression/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_compression -i testData/enwik8 -o testData/enwik8.bz2 
+    echo "OpenMP $WORD: $i / 10"
+    /usr/bin/time --append --output $OUTPUT_DIR/openmp_compression.csv --format=$TIME_FORMAT --quiet src/bin/openmp_compression -i testData/enwik8 -o testData/enwik8.bz2 
     rm testData/enwik8.bz2
 done
